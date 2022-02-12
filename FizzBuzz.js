fe13c92ade42edf1a1115cfef82cf40e5c9d5cdf
akshay-shah <akshay27991@gmail.com>
2022-02-13 05:16:04 +0530

    refactoring

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,11 +1,15 @@
-for (let index = 0; index <= 100; index++) {
-  if(index % 3 === 0 && index % 5 === 0){
+function fizzBuzz() {
+  for (let index = 1; index <= 100; index++) {
+    if (index % 3 === 0 && index % 5 === 0) {
       console.log("FizzBuzz");
-  }else if(index % 5 === 0){
+    } else if (index % 5 === 0) {
       console.log("Buzz");
-  }else if(index % 3 === 0){
+    } else if (index % 3 === 0) {
       console.log("Fizz");
-  }else {
+    } else {
       console.log(index);
+    }
   }
 }
+
+fizzBuzz();
